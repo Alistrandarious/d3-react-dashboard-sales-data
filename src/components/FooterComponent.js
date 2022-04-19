936a5e9ddb8b039b1c949e15def8a5bfa519f317
Ali Shah <SAMNS95@Gmail.com>
2022-04-19 17:02:52 +0100

    basic template set up

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,36 @@
+import React from "react";
+import primer from "./imgs/primerlogo.png";
+import github from "./imgs/githublogo.png";
+
+function Footer() {
+  const handleClick = (source) => {
+    if (source == "primer") {
+      window.open("https://primer.io/");
+    }
+    if (source == "github") {
+      window.open("https://github.com/Alistrandarious/d3-react-dashboard-sales-data");
+    }
+  };
+  return (
+    <footer class="mt-auto">
+      This project was complete by&nbsp;
+      <a href="https://www.linkedin.com/in/AliS/">Ali Shah</a>
+      <img
+        onClick={handleClick("primer")}
+        src={primer}
+        className="primer-logo logo"
+        alt="logo"
+        title="Click here to be taken to Primer.io"
+      />
+      <img
+        onClick={handleClick("github")}
+        src={github}
+        className="github-logo logo"
+        alt="logo"
+        title="Click here to be taken to the git repo"
+      />
+    </footer>
+  );
+}
+
+export default Footer;
